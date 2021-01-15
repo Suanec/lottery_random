a9791826beec1384f5bdc0d82925faef600e72a7
enzhao <enzhao@staff.weibo.com>
2021-01-15 18:47:23 +0800

    - [ENH] entrance by lottery_entrance.py.

--- lottery_entrance.py
+++ lottery_entrance.py
@@ -2,3 +2,39 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'lynne'
 # Created by lynne on 2021/1/13.
+
+import time
+import argparse
+from lottery_random import LotteryRandom
+
+class LotteryEntrance(object):
+    def __init__(self, _seed = 61833404L, _input_uid_file = "./input.uid", _restart_flag = False):
+        self.seed = _seed
+        self.input_uid_file = _input_uid_file
+        self.candidate_list = open(self.input_uid_file, "r").read().split("\n")
+        self.pre_winner = set()
+        self.lottery_rander = LotteryRandom(self.candidate_list, self.pre_winner, self.seed)
+        if(_restart_flag):
+            self.lottery_rander.stage_cleansing()
+            self.lottery_rander.reinit(self.candidate_list, self.pre_winner, self.seed)
+
+    def rand_perm(self, _level):
+        self.cur_lottery_result = self.lottery_rander.rander_perm(_level=_level)
+        print(self.cur_lottery_result)
+        self.pre_winner.update(set(self.cur_lottery_result))
+        return self.cur_lottery_result
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='random lottery entrance. ')
+    parser.add_argument('-f', '--input_uid_file', default="./input.uid", dest='input_uid_file', type=str,
+                        help='input_uid_file.')
+    parser.add_argument("-s", '--seed', dest='seed', type=str, default=long(time.time()),
+                        help="random seed.")
+    parser.add_argument("-l", '--level', dest='level', type=int, default=8,
+                        help="lottery level.")
+    args = parser.parse_args()
+
+    le = LotteryEntrance(_seed=args.seed, _input_uid_file=args.input_uid_file, _restart_flag=False)
+    print(le.rand_perm(args.level))
+
