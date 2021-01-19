4b1dd0f1ae6c618203c589fa8394489765b2ff88
enzhao <enzhao@staff.weibo.com>
2021-01-19 20:08:28 +0800

    - [RST] result of simple test.

---
+++ random_test_result/test.level0.100times.sh
@@ -0,0 +1,1 @@
+for i in `seq 1 100`; do python lottery_entrance.py -l 0 -c 2>&1 | grep level0;done; 
---
+++ random_test_result/test.level8.1000times.sh
@@ -0,0 +1,1 @@
+for i in `seq 1 1000`; do python lottery_entrance.py -l 8 -c 2>&1 | grep level8 ;done; 
